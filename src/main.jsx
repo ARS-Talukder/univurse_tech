01de5ff3881a17c3de88dca3133f78b9a6ad902f
ARS Talukder <talukderars@gmail.com>
2026-08-20 23:27:54 +0600

    App Loader implemented

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from 'react-hot-toast';
+import AppLoader from './components/Shared/AppLoader.jsx';
 
 
 const queryClient = new QueryClient();
@@ -11,7 +12,9 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <AppLoader>
+        <App />
+      </AppLoader>
       <Toaster
         position="top-right"
         reverseOrder={false}
